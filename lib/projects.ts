4e099bbf90841c3a4210edbddfed273e9cd092f6
Margaret Ajibola <margaretajibola56@yahoo.com>
2025-11-09 15:01:16 -0500

    updates to project details

--- lib/projects.ts
+++ lib/projects.ts
@@ -10,21 +10,21 @@ export const projects = [
   {
     title: "WigsByMagss Booking Website (In Progress)",
     description:
-      "A modern booking platform for salon services with an admin dashboard and scheduling system.",
-    tech: ["React", "Next.js", "Tailwind", "Prisma", "PostgresSQL", "TypeScript"],
+      "A modern booking platform for salon services with an admin/user dashboard and scheduling system.",
+    tech: ["React", "Next.js", "Tailwind CSS", "Prisma", "PostgresSQL", "TypeScript", "Playwright"],
     github: "https://github.com/margaretajibola/WigsbymagssBookingWebsite",
   },
    {
     title: "Museum of Black Women",
     description:
-      "A basic HTML + Bootstrap 5 website displaying pictures of Black women, created to highlight representation and diversity through visuals.",
-    tech: ["HTML", "Bootstrap 5"],
+      "A basic HTML + Bootstrap website displaying pictures of Black women, created to highlight representation and diversity through visuals.",
+    tech: ["HTML", "Bootstrap"],
     github: "https://github.com/margaretajibola/Museum-of-Black-Women",
   },
   {
     title: "Ping Pong Game",
-    description: "An interactive HTML, CSS, and JavaScript website that lets users play a classic pong-style game in the browser.",
-    tech: ["HTML", "CSS", "JavaScript"],
+    description: "An interactive HTML, Bulma (CSS Framework), and JavaScript website that lets users play a classic pong-style game in the browser.",
+    tech: ["HTML", "Bulma", "JavaScript"],
     github: "https://github.com/margaretajibola/Ping-Pong-Game",
   }
 ];
